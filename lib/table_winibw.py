6dec59f40e9ad8371fd30b5e07ca496cf3a67f2c
Hartmut Beyer <beyer@hab.de>
2020-12-10 21:46:36 +0100

    Automatisches Ergänzen Sortierform

--- lib/table_winibw.py
+++ lib/table_winibw.py
@@ -3,6 +3,8 @@
 
 import csv
 from lib import localsql as ls
+from lib import shelfmark as sm
+from lib import csvt
 
 # Verarbeitung von CSV-Tabellen, die in der WinIBW generiert und unter path abgelegt wurden
 class Table():
@@ -15,7 +17,8 @@ class Table():
 			return(None)
 		reader = csv.DictReader(file, delimiter=";")
 		self.content = [row for row in reader]
-		self.fields = [tup for tup in self.content[0] if tup != ""]
+		fieldDict = self.content.pop(0)
+		self.fields = [tup for tup in fieldDict if tup != ""]
 	def getByField(self, field):
 		ret = [row[field] for row in self.content]
 		return(ret)
@@ -27,6 +30,19 @@ class Table():
 		# Gibt die function None aus, wird die entsprechende Zeile entfernt
 		self.content = [row for row in self.content if row != None]
 		return(self.content)
+	def addSortable(self, field = "Signatur"):
+		if field not in self.fields:
+			print("Keine Spalte " + field + " gefunden.")
+			return(False)
+		self.fields.append("Sortierform")
+		for row in self.content:
+			sig = sm.StructuredShelfmark(row[field])
+			row["Sortierform"] = sig.sortable
+		return(True)
+	def save(self, fileName = "myTable"):
+		body = [[row[key] for key in row] for row in self.content]
+		table = csvt.Table(self.fields, body)
+		table.save(fileName)
 	def toSQLite(self, fileName = "exportTable"):
 		db = ls.Database(self.fields, [[row[field] for field in self.fields] for row in self.content], fileName)
-		return(True)
+		return(True)		

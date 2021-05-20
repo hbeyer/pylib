529c031298f8a406e916e4f94455da0b30bc0b7e
Hartmut Beyer <beyer@hab.de>
2021-05-20 20:36:55 +0200

    Weiterentwicklung PICA-Record

--- lib/pica.py
+++ lib/pica.py
@@ -64,6 +64,7 @@ class webReader(Reader):
 class Record:
 	def __init__(self, node):
 		self.node = node
+		self.persons = []
 		self.copies = []
 		try:
 			self.ppn = self.getValues("003@", "0").pop(0)
@@ -89,7 +90,8 @@ class Record:
 		except:
 			self.date = ""
 		self.shelfmarks = self.getValues("209A", "a")
-		self.loadCreators()
+		self.loadPersons()
+		self.loadCopies()
 	def __str__(self):
 		ret = "record: PPN " + self.ppn + ", VD17: " + "|".join(self.vd17) + ", Jahr: " + self.date
 		return(ret)
@@ -108,14 +110,51 @@ class Record:
 			else:
 				self.copies.append(copy)
 				occ += 1
-	def loadCreators(self):
+	def loadPersons(self):
 		subfields = ["7", "A", "D", "P", "L", "a", "d", "p", "l"]
 		creatorData = self.getNestedValues("028A", subfields)
+		if creatorData != {}:
+			per = Person()
+			per.role = "creator"
+			per.importPICA(creatorData)
+			self.persons.append(per)
 		creator2Data = self.getNestedValuesOcc("028B", subfields)
+		for row in creator2Data:
+			per = Person()
+			per.role = "creator"
+			per.importPICA(row)
+			self.persons.append(per)	
 		subfields.append("B")
-		# Das Folgende funktioniert so nicht, weil es hier keine occurrence gibt
 		contributorData = self.getNestedValuesMulti("028C", subfields)
-		print(contributorData)
+		for row in contributorData:
+			per = Person()
+			per.role = "contributor"
+			per.importPICA(row)
+			self.persons.append(per)
+	def loadCopies(self):
+		copyData = self.getNestedValuesOcc("209A", ["a"])
+		copyDataLib1 = self.getNestedValuesOcc("201D", ["0"])
+		copyDataLib2 = self.getNestedValuesOcc("202D", ["a"])
+		count = 0
+		for row in copyData:
+			sm = ""
+			isil = ""
+			try:
+				sm = row["a"]
+			except:
+				pass
+			try:
+				isil = copyDataLib2[count]["a"]
+			except:
+				try:
+					isil = copyDataLib1[count]["0"].split(":").pop(0)
+				except:
+					pass
+			cop = Copy(sm)
+			cop.isil = isil
+			self.copies.append(cop)
+			count += 1
+		#Über alle drei gemeinsam iterieren und Copy-Objekte anlegen
 	def getValues(self, field, subfield):
 		fields = self.node.findall(".//{info:srw/schema/5/picaXML-v1.0}datafield[@tag='" + field + "']/{info:srw/schema/5/picaXML-v1.0}subfield[@code='" + subfield + "']")
 		if fields:
@@ -135,7 +174,7 @@ class Record:
 		for fieldNode in fields:
 			for sf in subfields:
 				if fieldNode.attrib["code"] == sf:
-					row[sf] = fieldNode.text
+					row[sf.lower()] = fieldNode.text
 		return(row)
 	def getNestedValuesOcc(self, field, subfields):
 		ret = []
@@ -158,7 +197,7 @@ class Record:
 			row = {}
 			for sf in subfields:
 				try: 
-					row[sf] = fieldNode.find("{info:srw/schema/5/picaXML-v1.0}subfield[@code='" + sf + "']").text
+					row[sf.lower()] = fieldNode.find("{info:srw/schema/5/picaXML-v1.0}subfield[@code='" + sf + "']").text
 				except:
 					pass
 			ret.append(row)
@@ -169,13 +208,67 @@ class Record:
 			return([field.text.strip() for field in fields])
 		return([])
 
+class Person:
+	def __init__(self):
+		self.persName = ""
+		self.forename = ""
+		self.surname = ""
+		self.namePart1 = ""
+		self.namePart2 = ""
+		self.gnd = ""
+	def __str__(self):
+		if self.forename and self.surname:
+			self.persName = self.surname + ", " + self.forename
+		elif self.namePart1 and namePart2:
+			self.persName = self.namePart1 + " " + self.namePart2
+		elif self.namePart1:
+			self.persName = self.namePart1
+		ret = self.persName
+		try:
+			ret.append(" GND: " + self.gnd)
+		except:
+			pass
+		return(ret)
+	def importPICA(self, row):
+		try:
+			self.forename = row["d"]
+		except:
+			pass
+		try:
+			self.surname = row["a"]
+		except:
+			pass
+		try:
+			self.namePart1 = row["p"]
+		except:
+			pass
+		try:
+			self.namePart2 = row["l"]
+		except:
+			pass
+		try:
+			self.role = row["b"]
+		except:
+			pass
+		try:
+			self.gnd = row["7"].replace("gnd/", "")
+		except:
+			pass
+		return(True)
 class Copy:
 	def __init__(self, sm, provenances = [], epn = None):
+		self.place = ""
+		self.bib = ""
+		self.isil = ""
 		self.sm = sm
 		self.provenances = provenances
 		self.epn = epn
 	def __str__(self):
 		ret = "Signatur: " + self.sm
+		try:
+			ret = "ISIL: " + self.isil + " " + ret
+		except:
+			pass
 		try:
 			ret += ", Provenienz: " + ";".join(self.provenances)
 		except:

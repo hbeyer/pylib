aa824c71d95a4e8e99a9e33701a456eea47d29e0
Hartmut Beyer <beyer@hab.de>
2022-04-15 13:41:09 +0200

    Dünnhaupt

--- test.py
+++ test.py
@@ -2,23 +2,12 @@
 # -*- coding: utf-8 -*-
 
 
+import logging
+from lib import duennhaupt as dh
 from lib import evalpdf as ep
 
-#ev = ep.Evaluation("source/kataloge/Reiss-208.pdf", ["processionarium", "künrath"], [("Inkunabeln", "14\d\d")])
-ev = ep.EvaluationSDD("source/kataloge/Reiss-208.pdf")
+qww = dh.get_query_words()
+
+ev = ep.Evaluation("source/kataloge/jeschke141.pdf", qww)
 ev.eval()
 print(ev)
-
-"""
-import pdfplumber
-import PyPDF2
-import logging
-logging.basicConfig(level=logging.INFO)
-
-with pdfplumber.open("source/kataloge/Reiss-209.pdf") as pdf:
-    first_page = pdf.pages[23]
-    print(first_page.extract_text())
-"""
-
-# Dokumentation: https://github.com/jsvine/pdfplumber
-

1972e4b0a0d7c00c436a8fc9425a7f4f8dc2aa5d
beyer <beyer@hab.de>
2022-05-13 11:58:47 +0200

    GeoDataSheets in CSV

--- lib/csvt.py
+++ lib/csvt.py
@@ -6,12 +6,16 @@ import logging
 
 class Table:
     encoding = "utf-8"
-    def __init__(self, fields = [], content = []):
-        self.content = content
-        self.fields = fields
-    def save(self, path):
+    def __init__(self, fields = None, content = None):
+        self.content = []
+        self.fields = []
+        if content != None:
+            self.content = content
+        if fields != None:
+            self.fields = fields
+    def save(self, path, delimiter = ";"):
         with open(path + ".csv", 'w', encoding=self.encoding, newline="") as csvfile:
-            writer = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            writer = csv.writer(csvfile, delimiter=delimiter, quotechar='"', quoting=csv.QUOTE_MINIMAL)
             writer.writerow(self.fields)
             for row in self.content:
                 writer.writerow(row)
@@ -29,3 +33,59 @@ class Table:
         return(True)
 class TableWin(Table):
     encoding = "cp1252"
+class TableGeoBrowser(Table):
+    def __init__(self, content = None):
+        super().__init__(content)
+        self.fields = ["Name", "Address", "Description", "Longitude", "Latitude", "TimeStamp", "TimeSpan:begin", "TimeSpan:end", "GettyID", "Weight"]
+        if content != None:
+            for row in content:
+                self.import_row(row)
+    def import_row(self, row):
+        if type(row) != GeoDataRow:
+            logging.error(f"Ungültiger Geodatensatz übergeben: {str(row)}")
+            return(False)
+        self.content.append(row.to_list())
+    def simplify_content(self):
+        index = {}
+        for row in self.content:
+            try:
+                index[row[0] + row[1] + row[5]][9] += row[9]
+            except:
+                index[row[0] + row[1] + row[5]] = row
+        self.content = []
+        for ind, data in index.items():
+            self.content.append(data)
+    def fill_geo_data(self, gdb):
+        for row in self.content:
+            if row[3] == "" or row[4] == "":
+                row[3], row[4] = gdb.get_coord(row[0])
+        return(True)            
+    def save(self, path):
+        with open(path + ".csv", 'w', encoding=self.encoding, newline="") as csvfile:
+            writer = csv.writer(csvfile, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(self.fields)
+            for row in self.content:
+                writer.writerow(row)
+        return(True)
+class GeoDataRow:
+    def __init__(self, name=None, long=None, lat=None, timeStamp=None, weight=None):
+        self.name = ""
+        if name != None:
+            self.name = name
+        self.address = ""       
+        self.description = ""
+        self.long = ""
+        if long != None:
+            self.long = long
+        self.lat = ""
+        if lat != None:
+            self.lat = lat        
+        self.timeStamp = ""
+        if timeStamp != None:
+            self.timeStamp = timeStamp
+        self.getty = ""
+        self.weight = 1
+        if weight != None:
+            self.weight = weight
+    def to_list(self):
+        return([self.name, self.address, self.description, self.long, self.lat, self.timeStamp, "", "", self.getty, self.weight])
--- test.py
+++ test.py
@@ -4,36 +4,19 @@
 
 import logging
 import re
-from lib import table_winibw as tw
+from lib import csvt
+from lib import geo
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
 
-expr_umf = r"\(?\d+\)? (Bll\.|S\.)"
-expr_form = r"([248]|12)°"
+gdb = geo.DB()
 
-nost = tw.Table("source/Angebotsliste-Nostitz.csv")
-
-for count, row in enumerate(nost):
-    umf = ""
-    for tup, val in row.items():
-        extr = re.search(expr_umf, val)
-        if extr != None:
-            umf = extr.group(0)
-            break
-    form = ""
-    for tup, val in row.items():
-        extr = re.search(expr_form, val)
-        if extr != None:
-            form = extr.group(0)
-            break
-    if umf != "" and form != "":
-        koll = f"{umf}, {form}"
-    elif umf != "":
-        koll = umf
-    elif form != "":
-        koll = form
-    else:
-        koll = ""
-    print(koll)
-    
+testRow = csvt.GeoDataRow("Leipzig", "12.4167", "51.3333", "1650", 7)
+testRow2 = csvt.GeoDataRow("Dresden", "13.7500", "51.0500", "1670", 5)
+testRow3 = csvt.GeoDataRow("Leipzig", "12.4167", "51.3333", "1650", 20)
+testRow4 = csvt.GeoDataRow("Pirna", "", "", "1700", 3)
+gt = csvt.TableGeoBrowser([testRow, testRow2, testRow3, testRow4])
+gt.simplify_content()
+gt.fill_geo_data(gdb)
+gt.save("testGeo")

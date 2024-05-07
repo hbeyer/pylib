b4884617158a9b3a5317ff85b29211fce2c1173a
Hartmut Beyer <hartmut_beyer@gmx.de>
2024-05-07 17:48:41 +0200

    Update lobid.py

--- lib/lobid.py
+++ lib/lobid.py
@@ -17,12 +17,10 @@ class Request_Lobid():
         self.query_enc = ""
         self.num_found = 0
         self.data = []
-    def prepare(self, query):
-        self.query = query
-        if self.query == "":
-            return(None)
-        self.query_enc = up.quote(self.query)
+    def make_url(self):
         self.url = self.base + "?q=" + self.query_enc + "&size=" + self.size + "&format=" + self.format
+        return(True)
+    def get_num(self):
         lc = cache.CacheGNDLobid()
         text = lc.get_content(self.url, self.query)
         try:
@@ -36,11 +34,20 @@ class Request_Lobid():
             #logging.error(f"Fehler beim Laden des Ergebnisses für {self.query}")
             return(None)
         #else:
-            #logging.info(f"{self.num_found} Treffer für {self.query}")
+            #logging.info(f"{self.num_found} Treffer für {self.query}")        
+        return(True)
+    def prepare(self, query):
+        self.query = query
+        if self.query == "":
+            return(None)
+        self.query_enc = up.quote(self.query)
+        self.make_url()
+        self.get_num()
         return(True)
     def get_result(self):
         return(None)
 
+# Eine Suche in der vom hbz bereitgestellten lobid-gnd, s. http://lobid.org/gnd/
 class Request_GNDLobid(Request_Lobid):
     def __init__(self, size = None):
         super().__init__()
@@ -57,49 +64,77 @@ class Request_GNDLobid(Request_Lobid):
         for mem in self.data["member"]:
             if "Person" not in mem["type"]:
                 continue
-            info = { 
-                "preferredName" : mem["preferredName"], 
-                "gnd" : mem["gndIdentifier"], 
-                "variantNames" : "", 
-                "dateOfBirth" : "", 
-                "dateOfDeath" : "", 
-                "placeOfBirth" : "", 
-                "placeOfDeath" : "", 
-                "periodOfActivity" : "", 
-                "biographicalOrHistoricalInformation" : "", 
-                "gender" : ""
-            }
-            try:
-                info["variantNames"] = ";".join(mem["variantName"])
-            except:
-                pass
-            try:
-                info["dateOfBirth"] = mem["dateOfBirth"][0]
-            except:
-                pass
-            try:
-                info["dateOfDeath"] = mem["dateOfDeath"][0]
-            except:
-                pass
-            try:
-                info["placeOfBirth"] = mem["placeOfBirth"][0]["label"]
-            except:
-                pass
-            try:
-                info["placeOfDeath"] = mem["placeOfDeath"][0]["label"]
-            except:
-                pass
-            try:
-                info["periodOfActivity"] = "; ".join(mem["periodOfActivity"])
-            except:
-                pass
-            try:
-                info["biographicalOrHistoricalInformation"] = "; ".join(mem["biographicalOrHistoricalInformation"])
-            except:
-                pass
-            try:
-                info["gender"] = mem["gender"][0]["label"]
-            except:
-                pass
-            result.append(info)
+            result.append(self.extract_info(mem))
+        return(result)
+    def extract_info(self, mem):
+        info = { 
+            "preferredName" : mem["preferredName"], 
+            "gnd" : mem["gndIdentifier"], 
+            "variantNames" : "", 
+            "dateOfBirth" : "", 
+            "dateOfDeath" : "", 
+            "placeOfBirth" : "", 
+            "placeOfDeath" : "", 
+            "periodOfActivity" : "", 
+            "biographicalOrHistoricalInformation" : "", 
+            "gender" : ""
+        }
+        try:
+            info["variantNames"] = ";".join(mem["variantName"]).replace('"', '').replace("„", "").replace("“", "")
+        except:
+            pass
+        try:
+            info["dateOfBirth"] = mem["dateOfBirth"][0]
+        except:
+            pass
+        try:
+            info["dateOfDeath"] = mem["dateOfDeath"][0]
+        except:
+            pass
+        try:
+            info["placeOfBirth"] = mem["placeOfBirth"][0]["label"]
+        except:
+            pass
+        try:
+            info["placeOfDeath"] = mem["placeOfDeath"][0]["label"]
+        except:
+            pass
+        try:
+            info["periodOfActivity"] = "; ".join(mem["periodOfActivity"])
+        except:
+            pass
+        try:
+            info["biographicalOrHistoricalInformation"] = "; ".join(mem["biographicalOrHistoricalInformation"])
+        except:
+            pass
+        try:
+            info["gender"] = mem["gender"][0]["label"]
+        except:
+            pass
+        return(info)        
+
+# Eine Suche, bei der eine GND-Nummer übergeben wird
+class Request_GNDLobid_ID(Request_GNDLobid):
+    def __init__(self, size = None):
+        super().__init__()
+    def make_url(self):
+        self.url = f"https://lobid.org/gnd/{self.query_enc}.json"
+        return(True)
+    def get_num(self):
+        lc = cache.CacheGNDLobid()
+        text = lc.get_content(self.url, self.query)
+        try:
+            self.data = json.loads(text)
+        except:
+            #logging.error(f"Fehler beim Laden des Ergebnisses für {self.query}")
+            pass
+        if len(self.data) > 0:
+            self.num_found = 1
+            return(True)
+        return(False)
+    def get_result(self):
+        result = []
+        if self.num_found == 0:
+            return(result)
+        result.append(self.extract_info(self.data))
         return(result)
